6ffa79ab28678505c0ed5b052b5afe94acb10cd1
Guilherme S Beneti <guilhermesbeneti@Guilhermes-MBP.local>
2020-09-17 19:18:56 -0300

    month and year filter implemented

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,9 +7,12 @@ import FinanceHistoryCard from '../../components/FinanceHistoryCard'
 
 import gains from '../../repositories/gains'
 import expenses from '../../repositories/expenses'
+import monthsList from '../../utils/months'
 
 import formatCurrency from '../../utils/formatCurrency'
 import formatDate from '../../utils/formatDate'
+import { v4 as uuidv4 } from 'uuid'
+
 
 interface IRouteParams {
     match: {
@@ -49,35 +52,34 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         return type === 'money-in' ? gains : expenses
     }, [type])
 
-    const months = [
-        {
-            value: 1, label: 'Janeiro',
-        },
-        {
-            value: 7, label: 'Julho',
-        },
-        {
-            value: 9, label: 'Setembro',
-        },
-        {
-            value: 10, label: 'Outubro'
-        },
-        {
-            value: 11, label: 'Novembro'
-        }
-    ]
+    const months = useMemo(() => {
+        return monthsList.map((month, index) => {
+            return {
+                value: index + 1,
+                label: month
+            }
+        })
+    }, [])
 
-    const years = [
-        {
-            value: 2020, label: 2020,
-        },
-        {
-            value: 2021, label: 2021
-        },
-        {
-            value: 2022, label: 2022
-        }
-    ]
+    const years = useMemo(() => {
+        let uniqueYears: number[] = []
+
+        listData.forEach(item => {
+            const date = new Date(item.date)
+            const year = date.getFullYear()
+
+            if(!uniqueYears.includes(year)) {
+                uniqueYears.push(year)
+            }
+        })
+
+        return uniqueYears.map(year => {
+            return {
+                value: year,
+                label: year
+            }
+        })
+    }, [listData])
 
     useEffect(() => {
         const filteredDate = listData.filter(item => {
@@ -91,7 +93,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
 
         const formatedDate = filteredDate.map(item => {
             return {
-                id: String(new Date().getTime()) + item.amount,
+                id: uuidv4(),
                 description: item.description,
                 amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
