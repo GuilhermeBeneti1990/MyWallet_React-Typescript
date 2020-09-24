8fe3c136859651c87e24160fabe1cd1b0efc7189
Guilherme S Beneti <guilhermesbeneti@Guilhermes-MBP.local>
2020-09-24 16:57:28 -0300

    Theme toggle implemented

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useCallback } from 'react'
 import { Container, Content } from './styles'
 
 import ContentHeader from '../../components/ContentHeader'
@@ -281,23 +281,23 @@ const Dashboard: React.FC = () => {
         ]
     }, [yearSelected, monthSelected])
 
-    const handleMonthSelected = (month: string) => {
+    const handleMonthSelected = useCallback((month: string) => {
         try {
             const parsedMonth = Number(month)
             setMonthSelected(parsedMonth)
         } catch(error) {
             throw new Error('Invalid month value!')
         }
-    }
+    }, [])
 
-    const handleYearSelected = (year: string) => {
+    const handleYearSelected = useCallback((year: string) => {
         try {
             const parsedYear = Number(year)
             setYearSelected(parsedYear)
         } catch(error) {
             throw new Error('Invalid year value!')
         }
-    }
+    }, [])
 
     return (
         <Container>
